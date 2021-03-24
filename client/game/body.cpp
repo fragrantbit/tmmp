8e8f253ed58cf130ebdcbb5769602d62d01b7925
fragrantbit <42542754+fragrantbit@users.noreply.github.com>
2021-03-24 12:43:07 +0200

    Broken english fix

--- client/game/body.cpp
+++ client/game/body.cpp
@@ -13,8 +13,7 @@ CBody::CBody(int iTypeOrID)
 	pBody = new newBody;
 
 	/* Available skins begin from at least 0 identifier, otherwise game's crash is guaranteed. 
-	   It's hard or most likely not possible to make the game be loading a < 0 identifier actor model over the multiplayer net mechanism, 
-	   so it's not sensible to let it happen.
+	   It's not possible to load a < 0 model identifier, so it's not sensible to let it happen.
 	*/
 	if(iTypeOrID < 0)
 		iTypeOrID = 0; 
